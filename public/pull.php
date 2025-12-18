a2cf3d2c1c5860e1ce45314a0188dc60dd1319df
Haseeb hanif <haseeb.idevation@gmail.com>
2025-12-18 18:18:04 +0500

    Update pull.php

--- public/pull.php
+++ public/pull.php
@@ -1,3 +1,1 @@
-<?php echo exec("git pull https://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@github.com/ffaady/Visiolims.git master");
-//git clone https://Haseebhanif:Idevation786@github.com/ffaady/Visiolims.git master
-?>
+
